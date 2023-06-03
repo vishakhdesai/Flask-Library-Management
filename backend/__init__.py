8cd7ad1c8368502bb418ddd7a2e683c8575b90e6
Vishakh <vishakhdesai@gmail.com>
2023-06-03 11:24:25 +0530

    updated db url

--- backend/__init__.py
+++ backend/__init__.py
@@ -8,7 +8,7 @@ from .routes.index import index
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password@localhost:5433/library-records"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://vishakhdesai:q8I7X5M4ae8L1nQDhJcoU3Cw9W7zEap1@dpg-chtd485269vccp4sugd0-a.oregon-postgres.render.com/library_records"
     db.init_app(app)
     migrate.init_app(app,db)
     app.register_blueprint(member)
